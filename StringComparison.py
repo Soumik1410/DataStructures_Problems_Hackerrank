74205d6204d52bed89ef02d24bc59a8210a1cd04
SoumikCoding <soumik1410code@gmail.com>
2024-08-07 19:57:03 +0530

    Create StringComparison.py

---
+++ StringComparison.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    # Write your code here
+    result = []
+    stringList_count = len(stringList)
+    queries_count = len(queries)
+    for i in range(queries_count):
+        sum=0
+        for j in range(stringList_count):
+            if queries[i] == stringList[j]:
+                sum+=1
+        result.append(sum)
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
